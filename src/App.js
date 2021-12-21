b8e63ad82f0dcd31201b35bce024250bfaa0aa0a
narcelio <narcelio16@hotmail.com>
2021-12-21 20:51:55 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+
+import { useEffect, useState } from 'react';
+import axios from 'axios'
 import './App.css';
 
 function App() {
+  const [livros, setLivros] =useState([])
+  const [busca, setBusca] = useState('')
+  //const livrosFiltrados = livros.filter((livro)=> livro.startsWith(busca))
+  useEffect(()=>{
+    const params = {}
+    if(busca){
+      params.title = busca
+    } 
+    axios.get('http://hn.algolia.com/api/v1/search?query=foo&tags=',{params}).then(({data})=>{
+        setLivros(data.hits)
+       
+    })
+  },[busca])
+  
+ 
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='main'>
+      <h2>{process.env.REACT_APP_TITLE}</h2>
+      <form>
+      <input 
+      type='text' 
+      value={busca} 
+      onChange={(ev) => setBusca(ev.target.value)} 
+      placeholder='pesquisar...'/>
+      
+     </form>
+    {livros?.map((livro)=>(
+      <div className='list' >
+        <div className='text'>
+        <h3>👥{livro.author}</h3>
+        <h3>📖{livro.title}</h3>
+        <a href={livro.url}>🔗Ver mais...</a>
+        </div>
+      </div>
+
+    ))}
+        
+    
     </div>
-  );
+  )
 }
 
 export default App;
